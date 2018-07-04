43a08097755cc4f145b9c58a0a1561384cfb32ef
EthanLin <ethan510010@gmail.com>
2018-07-04 19:59:21 +0800

    fix selectedButton

--- Mopcon/Mopcon/Controller/CommunityViewController.swift
+++ Mopcon/Mopcon/Controller/CommunityViewController.swift
@@ -35,7 +35,7 @@ class CommunityViewController: UIViewController {
     func changeButtonColor(beTappedButton:CustomSelectedButton, notSelectedButton:CustomSelectedButton){
           beTappedButton.backgroundColor = UIColor(red: 0, green: 208/255, blue: 203/255, alpha: 0.2)
           beTappedButton.setTitleColor(UIColor(red: 0, green: 208/255, blue: 203/255, alpha: 1), for: .normal)
-        
+          beTappedButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
           notSelectedButton.backgroundColor = UIColor.clear
           notSelectedButton.setTitleColor(UIColor(red: 0, green: 208/255, blue: 203/255, alpha: 0.5), for: .normal)
     }
