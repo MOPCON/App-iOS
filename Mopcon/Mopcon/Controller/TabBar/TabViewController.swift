09989dfbb2a3344f4bae3103face3ee5264cc9f4
Luke <wuchiwei17@gmail.com>
2019-07-17 11:47:37 +0800

    Add Lobby.storyboard for new UI requirement.
    modified:   Mopcon/Mopcon.xcodeproj/project.pbxproj
    [Reason] Add new file Lobby.Storyboard, TabViewController and ColorSet folder in Assets.
    
    new file:   Mopcon/Mopcon/Assets.xcassets/Color/Contents.json
    renamed:    Mopcon/Mopcon/Assets.xcassets/themeColor.colorset/Contents.json -> Mopcon/Mopcon/Assets.xcassets/Color/themeColor.colorset/Contents.json
    [Reason] Add Color folder in asset for manage color theme.
    
    new file:   Mopcon/Mopcon/Controller/TabBar/TabViewController.swift
    renamed:    Mopcon/Mopcon/Storyboard/Base.lproj/Main.storyboard -> Mopcon/Mopcon/Storyboard/Base.lproj/Lobby.storyboard
    new file:   Mopcon/Mopcon/Storyboard/Main.storyboard
    [Reason] Add new Storyboard for new UI requirement.

---
+++ Mopcon/Mopcon/Controller/TabBar/TabViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabViewController.swift
+//  Mopcon
+//
+//  Created by WU CHIH WEI on 2019/7/17.
+//  Copyright © 2019 EthanLin. All rights reserved.
+//
+
+import UIKit
+
+class TabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
