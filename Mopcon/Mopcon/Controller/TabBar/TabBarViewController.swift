3a88d98af9e32ed62347bde4b6351a15644910c2
Luke <wuchiwei17@gmail.com>
2019-07-17 15:40:40 +0800

    1. Add TabBarController and Main.storyboard for tab UI. 2. Add tab relative image in Assets 3. Add UIImage extension for image usage. 4. Add UIColor extension for color usage.

---
+++ Mopcon/Mopcon/Controller/TabBar/TabBarViewController.swift
@@ -0,0 +1,138 @@
+//
+//  TabViewController.swift
+//  Mopcon
+//
+//  Created by WU CHIH WEI on 2019/7/17.
+//  Copyright © 2019 EthanLin. All rights reserved.
+//
+
+import UIKit
+
+enum TabCategory: String {
+    
+    case lobby = "Lobby"
+    case schedual = "Schedual"
+    case missions = "Missions"
+    case news = "News"
+    case communication = "Communication"
+    case speaker = "Speaker"
+    case sponsor = "Sponsor"
+    case group = "Group"
+
+    func storyboard() -> UIStoryboard {
+        
+        return UIStoryboard(name: self.rawValue, bundle: nil)
+    }
+    
+    func instantiateInitialViewController() -> UIViewController {
+        
+        guard let vc = storyboard().instantiateInitialViewController() else {
+            
+            return UIViewController()
+        }
+        
+        //等到所有 tab 都完成，這邊就可以改成 return vc，不需要 switch case
+        switch self {
+            
+        case .lobby: return vc
+            
+        default: return UIViewController()
+            
+        }
+    }
+    
+    func image() -> UIImage? {
+        
+        switch self {
+        
+        case .lobby: return UIImage.asset(.lobby)
+            
+        case .schedual: return UIImage.asset(.schedual)
+            
+        case .missions: return UIImage.asset(.mission)
+            
+        case .news: return UIImage.asset(.news)
+            
+        case .communication: return UIImage.asset(.communication)
+            
+        case .speaker: return UIImage.asset(.speaker)
+            
+        case .sponsor: return UIImage.asset(.sponsor)
+            
+        case .group: return UIImage.asset(.group)
+            
+        }
+    }
+    
+    func seletedImage() -> UIImage? {
+        
+        switch self {
+            
+        case .lobby: return UIImage.asset(.lobbySelected)
+            
+        case .schedual: return UIImage.asset(.schedualSelected)
+            
+        case .missions: return UIImage.asset(.mission)
+            
+        case .news: return UIImage.asset(.newsSelected)
+            
+        case .communication: return UIImage.asset(.communication)
+            
+        case .speaker: return UIImage.asset(.speaker)
+            
+        case .sponsor: return UIImage.asset(.sponsor)
+            
+        case .group: return UIImage.asset(.group)
+            
+        }
+    }
+    
+    func title() -> String {
+        
+        switch self {
+            
+        case .lobby: return "首頁"
+            
+        case .schedual: return "議程"
+            
+        case .missions: return "任務"
+            
+        case .news: return "最新"
+            
+        case .communication: return "交流場次"
+            
+        case .speaker: return "講者介紹"
+            
+        case .sponsor: return "贊助廠商"
+            
+        case .group: return "社群"
+            
+        }
+    }
+}
+
+class TabBarViewController: UITabBarController {
+    
+    let tabs: [TabCategory] = [.lobby, .schedual, .missions, .news, .communication, .speaker, .sponsor, .group]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = tabs.map{ tab in
+            
+            let vc = tab.instantiateInitialViewController()
+            
+            let tabBarItem = UITabBarItem(title: tab.title(), image: tab.image(), selectedImage: tab.seletedImage())
+        
+            vc.tabBarItem = tabBarItem
+            
+            return vc
+        }
+        
+        tabBar.tintColor = UIColor.azure
+        
+        tabBar.barTintColor = UIColor.dark
+
+        tabBar.isTranslucent = false
+    }
+}
---
+++ Mopcon/Mopcon/Helper/Exntension/UIImageExtension.swift
@@ -0,0 +1,45 @@
+//
+//  UIImageExtension.swift
+//  Mopcon
+//
+//  Created by WU CHIH WEI on 2019/7/17.
+//  Copyright © 2019 EthanLin. All rights reserved.
+//
+
+import UIKit
+
+enum ImageAsset: String {
+    
+    case lobby = "lobby"
+    case lobbySelected = "lobby_selected"
+    
+    case schedual = "schedual"
+    case schedualSelected = "schedual_selected"
+    
+    case mission = "mission"
+    case missionSelected = "mission_selected"
+    
+    case news = "news"
+    case newsSelected = "news_selected"
+    
+    case more = "more"
+    case moreSelected = "more_selected"
+    
+    case communication = "communication"
+    
+    case speaker = "speaker"
+    
+    case sponsor = "sponsor"
+    
+    case group = "group"
+}
+
+extension UIImage {
+    
+    class func asset(_ type: ImageAsset) -> UIImage? {
+        
+        return UIImage(named: type.rawValue)
+    }
+}
+
+
