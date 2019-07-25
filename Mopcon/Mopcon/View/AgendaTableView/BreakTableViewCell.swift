968cc1641b92387cd5aca1b16d2ac906b7446654
Yang Tun-Kai <ks97121@gmail.com>
2019-07-25 22:47:25 +0800

    Update UI of conference and break cell

--- Mopcon/Mopcon/View/AgendaTableView/BreakTableViewCell.swift
+++ Mopcon/Mopcon/View/AgendaTableView/BreakTableViewCell.swift
@@ -12,27 +12,40 @@ class BreakTableViewCell: UITableViewCell {
     
     @IBOutlet weak var breakStepLabel: UILabel!
     
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func awakeFromNib() {
+        
         super.awakeFromNib()
-
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
+        
         super.setSelected(selected, animated: animated)
+        
         self.selectionStyle = .none
+        
         self.isUserInteractionEnabled = false
-        // Configure the view for the selected state
     }
     
     func updateUI(agenda:Schedule.Payload.Agenda.Item.AgendaContent) {
         
+        timeLabel.text = agenda.duration
+    
         let language = CurrentLanguage.getLanguage()
+        
         switch language {
+        
         case Language.chinese.rawValue:
-            self.breakStepLabel.text = agenda.schedule_topic
+        
+            breakStepLabel.text = agenda.schedule_topic
+            
         case Language.english.rawValue:
-            self.breakStepLabel.text = agenda.schedule_topic_en
+            
+            breakStepLabel.text = agenda.schedule_topic_en
+        
         default:
+        
             break
         }
     }
