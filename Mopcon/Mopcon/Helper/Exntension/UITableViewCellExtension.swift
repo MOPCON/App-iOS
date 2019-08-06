19b41f7871f78aa2dc255e13ccfe5b85e90583cc
Luke <wuchiwei17@gmail.com>
2019-08-06 20:44:14 +0800

    Create MoreViewController for more tab in TabbarViewController.
    [Modify] TabbarViewController
    [Reason] Move communication, speaker, sponsor, group view controllers to MoreViewController.

---
+++ Mopcon/Mopcon/Helper/Exntension/UITableViewCellExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UITableViewCellExtension.swift
+//  Mopcon
+//
+//  Created by WU CHIH WEI on 2019/8/6.
+//  Copyright © 2019 EthanLin. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    
+    static var identifier: String {
+        
+        return String(describing: self)
+    }
+}
+
