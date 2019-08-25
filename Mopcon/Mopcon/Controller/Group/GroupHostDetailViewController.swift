a9fe76b61fb4ac3867c556a97dfe82a11e1a02b5
Luke <wuchiwei17@gmail.com>
2019-08-25 17:59:10 +0800

    Update_Community_UI 1. Add social media icon in GroupHostDetailViewController. 2. Add scroll view in GroupHostDetailViewController.

--- Mopcon/Mopcon/Controller/Group/GroupHostDetailViewController.swift
+++ Mopcon/Mopcon/Controller/Group/GroupHostDetailViewController.swift
@@ -22,6 +22,10 @@ class GroupHostDetailViewController: MPBaseViewController {
     
     @IBOutlet weak var facebookButton: UIButton!
     
+    @IBOutlet weak var socialMediaStackView: UIStackView!
+    
+    let scrollView = UIScrollView()
+    
     @IBAction func connectToFacebook(_ sender: UIButton) {
         
         if let organizer = organizer, let url = URL(string: organizer.facebook) {
@@ -40,6 +44,74 @@ class GroupHostDetailViewController: MPBaseViewController {
         facebookButton.layer.borderColor = UIColor.azure?.cgColor
         
         facebookButton.layer.borderWidth = 1.0
+        
+        setupLayout()
+    }
+    
+    func setupLayout() {
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor)
+        ])
+        
+        communityDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        scrollView.addSubview(communityDescriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            communityDescriptionLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+            communityDescriptionLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20),
+            communityDescriptionLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 214),
+            communityDescriptionLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -244),
+            communityDescriptionLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1.0, constant: -40)
+        ])
+        
+        socialMediaStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        scrollView.addSubview(socialMediaStackView)
+    
+        NSLayoutConstraint.activate([
+            socialMediaStackView.centerXAnchor.constraint(equalTo: communityDescriptionLabel.centerXAnchor),
+            socialMediaStackView.bottomAnchor.constraint(equalTo: communityDescriptionLabel.topAnchor, constant: -16)
+        ])
+        
+        facebookButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        scrollView.addSubview(facebookButton)
+        
+        NSLayoutConstraint.activate([
+            facebookButton.centerXAnchor.constraint(equalTo: communityDescriptionLabel.centerXAnchor),
+            facebookButton.topAnchor.constraint(equalTo: communityDescriptionLabel.bottomAnchor, constant: 76),
+            facebookButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 172/375),
+            facebookButton.heightAnchor.constraint(equalTo: facebookButton.widthAnchor, multiplier: 52/172)
+        ])
+        
+        communityNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        scrollView.addSubview(communityNameLabel)
+        
+        NSLayoutConstraint.activate([
+            communityNameLabel.centerXAnchor.constraint(equalTo: communityDescriptionLabel.centerXAnchor),
+            communityNameLabel.bottomAnchor.constraint(equalTo: socialMediaStackView.topAnchor, constant: -16)
+        ])
+        
+        communityDetailImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        scrollView.addSubview(communityDetailImageView)
+        
+        NSLayoutConstraint.activate([
+            communityDetailImageView.centerXAnchor.constraint(equalTo: communityDescriptionLabel.centerXAnchor),
+            communityDetailImageView.heightAnchor.constraint(equalToConstant: 80),
+            communityDetailImageView.widthAnchor.constraint(equalTo: communityDetailImageView.heightAnchor),
+            communityDetailImageView.bottomAnchor.constraint(equalTo: communityNameLabel.topAnchor, constant: -26)
+        ])
     }
     
     override func viewDidLayoutSubviews() {
