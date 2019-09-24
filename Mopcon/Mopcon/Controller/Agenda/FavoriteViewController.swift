bbf5cfee72d2fbd0bdd61958086be366a6160e9b
Luke Wu <wuchiwei17@gmail.com>
2019-09-24 21:09:22 +0800

    Create FavoriteViewController for Favorite Session Scene.
    modified:   Mopcon.xcodeproj/project.pbxproj
    new file:   Mopcon/Controller/Agenda/FavoriteViewController.swift
    modified:   Mopcon/Storyboard/Agenda.storyboard

---
+++ Mopcon/Mopcon/Controller/Agenda/FavoriteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteViewController.swift
+//  Mopcon
+//
+//  Created by WU CHIH WEI on 2019/9/24.
+//  Copyright © 2019 EthanLin. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: MPBaseSessionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
