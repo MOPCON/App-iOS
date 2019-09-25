736c9f9c3b3fab13b06e5956395ce1ba178a33ab
Luke Wu <wuchiwei17@gmail.com>
2019-09-25 16:56:03 +0800

    Complete 交流場次 Tab。

--- Mopcon/Mopcon/Controller/CommunicationViewController.swift
+++ Mopcon/Mopcon/Controller/CommunicationViewController.swift
@@ -9,130 +9,30 @@
 import UIKit
 
 class CommunicationViewController: MPBaseViewController {
-   
-    var selectedSchedule = [Schedule_unconf.Payload.Item]()
     
-    var schedule_day1 = [Schedule_unconf.Payload.Item]()
+    private struct Segue {
+        
+        static let unconf = "SegueUnConf"
+    }
     
-    var schedule_day2 = [Schedule_unconf.Payload.Item]()
-    
-    let spinner = LoadingTool.setActivityindicator()
+    private var unconfVC: UnConferenceViewController?
 
     @IBOutlet weak var dateSelectionView: SelectionView!
     
-    @IBOutlet weak var communicationTableView: UITableView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setupTableView()
         
         dateSelectionView.dataSource = self
-        
-        getCommunicationAPI()
     }
     
-    private func setupTableView() {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        communicationTableView.separatorStyle = .none
-        
-        communicationTableView.delegate = self
-        
-        communicationTableView.dataSource = self
-        
-        let communicationCell = UINib(nibName: String(describing: CommunicationConferenceTableViewCell.self), bundle: nil)
-        
-        let communicationBreakCell = UINib(nibName: String(describing: CommunicationBreakTableViewCell.self), bundle: nil)
-        
-        communicationTableView.register(communicationCell, forCellReuseIdentifier: CommunicationTableViewCellID.communicationConferenceCell)
-        
-        communicationTableView.register(communicationBreakCell, forCellReuseIdentifier: CommunicationTableViewCellID.communicationBreakCell)
-    }
-    
-    func getCommunicationAPI() {
-        spinner.center = view.center
-        
-        spinner.startAnimating()
-        
-        self.view.addSubview(spinner)
-        
-        Schedule_unconfAPI.getAPI(url: MopconAPI.shared.schedule_unconf) { [weak self] (payload, error) in
+        if segue.identifier == Segue.unconf {
             
-            if error != nil {
-                print(error!.localizedDescription)
-                
-                self?.spinner.removeFromSuperview()
-                
-                return
-            }
+            guard let destination = segue.destination as? UnConferenceViewController else { return }
             
-            if let payload = payload {
-                
-                self?.schedule_day1 = payload[0].items
-                
-                self?.schedule_day2 = payload[1].items
-                
-                self?.selectedSchedule = self?.schedule_day1 ?? []
-                
-                DispatchQueue.main.async {
-                    self?.communicationTableView.reloadData()
-                    
-                    self?.spinner.removeFromSuperview()
-                }
-                
-            }
+            unconfVC = destination
         }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        
-        super.viewWillAppear(animated)
-        
-        if CurrentLanguage.getLanguage() == Language.english.rawValue {
-            
-            navigationItem.title = "Communication"
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        
-        super.didReceiveMemoryWarning()
-    }
-}
-extension CommunicationViewController: UITableViewDelegate, UITableViewDataSource{
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return selectedSchedule.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let schedule = selectedSchedule[indexPath.row]
-        
-        switch schedule.type {
-            
-        case "others":
-            
-            let communicationBreakCell = tableView.dequeueReusableCell(withIdentifier: CommunicationTableViewCellID.communicationBreakCell, for: indexPath) as! CommunicationBreakTableViewCell
-            communicationBreakCell.updateUI(schedule: schedule)
-            
-            return communicationBreakCell
-            
-        default:
-            
-            let communicationConferenceCell = tableView.dequeueReusableCell(withIdentifier: CommunicationTableViewCellID.communicationConferenceCell, for: indexPath) as! CommunicationConferenceTableViewCell
-            
-            communicationConferenceCell.updateUI(schedule: schedule)
-            
-            return communicationConferenceCell
-        }
-        
     }
 }
 
@@ -145,8 +45,6 @@ extension CommunicationViewController: SelectionViewDataSource {
     
     func didSelectedButton(_ selectionView: SelectionView, at index: Int) {
         
-        selectedSchedule = (index == 0) ? schedule_day1 : schedule_day2
-        
-        communicationTableView.reloadData()
+        unconfVC?.selectedIndex = index
     }
 }
