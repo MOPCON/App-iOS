b9bdcb8e8b78f76aad03f4ea348821d12968c489
Yang Tun-Kai <ks97121@gmail.com>
2020-09-26 23:48:08 +0800

    Update initial api

--- Mopcon/Mopcon/API/InitialAPI/InitialProvider.swift
+++ Mopcon/Mopcon/API/InitialAPI/InitialProvider.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class InitialProvider: MainThreadHelper {
     
-    static func fetchInitialAPI(completion: @escaping (Result<ServerState, Error>) -> Void) {
+    static func fetchInitialAPI(completion: @escaping InitialResultType) {
         
         HTTPClient.shared.request(InitialAPI.initial, completion: { result in
             
@@ -28,6 +28,38 @@ class InitialProvider: MainThreadHelper {
                     throwToMainThreadAsync {
                         
                         completion(Result.success(successResponse.data))
+                    }
+                    
+                } catch {
+                    
+                    completion(Result.failure(error))
+                }
+                
+            case .failure(let error):
+                
+                completion(Result.failure(error))
+            }
+        })
+    }
+    
+    static func fetchAppVersion(of bundleID: String, completion: @escaping VersionResultType) {
+
+        HTTPClient.shared.request(InitialAPI.version(bundleID), completion: { result in
+            
+            switch result{
+                
+            case .success(let data):
+                  
+                do{
+                    
+                    let successResponse = try JSONDecoder.shared.decode(
+                        VersionResponse<Version>.self,
+                        from: data
+                    )
+             
+                    throwToMainThreadAsync {
+                        
+                        completion(Result.success(successResponse.results))
                     }
                     
                 } catch {
