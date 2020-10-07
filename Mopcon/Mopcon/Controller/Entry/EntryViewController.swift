879dc4c100c2f5aa6606a437d1395d06d73bbda8
Yang Tun-Kai <ks97121@gmail.com>
2020-10-08 01:23:38 +0800

    Move check app version code

--- Mopcon/Mopcon/Controller/Entry/EntryViewController.swift
+++ Mopcon/Mopcon/Controller/Entry/EntryViewController.swift
@@ -30,7 +30,7 @@ class EntryViewController: UIViewController, MainThreadHelper {
                 let storyboard = UIStoryboard(name: "Main", bundle: nil)
                 
                 let tabVC = storyboard.instantiateViewController(withIdentifier: TabBarViewController.identifier)
-                
+           
                 UIView.transition(
                     with: window,
                     duration: 0.3,
@@ -49,7 +49,7 @@ class EntryViewController: UIViewController, MainThreadHelper {
                         items[2].isEnabled = serverState.isEnableGame
                     }
                 }
-                
+
                 if !UserDefaults.standard.bool(forKey: "hasOpened") {
                     
                     UserDefaults.standard.set(true, forKey: "hasOpened")
@@ -71,67 +71,9 @@ class EntryViewController: UIViewController, MainThreadHelper {
         })
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        checkAppVersion()
-    }
-    
     private func register(with uid: String, and email: String) {
         
         FieldGameProvider.register(with: uid, and: email)
     }
-    
-    private func checkAppVersion() {
         
-        guard let info = Bundle.main.infoDictionary, let bundleID = info["CFBundleIdentifier"] as? String, let currentVersion = info["CFBundleShortVersionString"] as? String else {
-            
-            return
-        }
-        
-        InitialProvider.fetchAppVersion(of: bundleID, completion: { [weak self] result in
-            
-            switch result {
-                
-            case .success(let version):
-                
-                self?.throwToMainThreadAsync {
-                    
-                    if currentVersion != version.first?.version {
-
-                        self?.showVersionAlert()
-                    }
-
-                }
-                
-            case .failure(let error):
-                
-                print(error)
-            }
-            
-            
-        })
-    }
-    
-    private func showVersionAlert() {
-        
-        let alert = UIAlertController(title: "提示", message: "目前已有新版 App，請前往 AppStore 更新。", preferredStyle: .alert)
-        
-        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
-        
-        let okAction = UIAlertAction(title: "前往 AppStore", style: .default, handler: { _ in
-            
-            if let url = URL(string: MPConstant.mopconAppStore) {
-                
-                UIApplication.shared.open(url)
-            }
-        })
-        
-        alert.addAction(cancelAction)
-        
-        alert.addAction(okAction)
-        
-        self.present(alert, animated: true, completion: nil)
-    }
-    
 }
