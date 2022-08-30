78e6bc0cafe89fa989f78b74ae511f6bf2759668
HongYi <linhongyi73@gmail.com>
2022-08-31 01:35:24 +0800

    贊助商詳細頁面加入網站, FB 按鈕顯示

--- Mopcon/Mopcon/View/Sponsor/SponsorInfoCell.swift
+++ Mopcon/Mopcon/View/Sponsor/SponsorInfoCell.swift
@@ -14,15 +14,44 @@ class SponsorInfoCell: UITableViewCell {
     
     @IBOutlet weak var companyLabel: UILabel!
     
+    @IBOutlet weak var webButton: UIButton!
+    
+    @IBOutlet weak var fbButton: UIButton!
+    
+    @IBOutlet weak var webButtonCenterLayoutConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var fbButtonCenterlayoutConstraint: NSLayoutConstraint!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        logoImageView.contentMode = UIView.ContentMode.scaleAspectFit
+        logoImageView.backgroundColor = UIColor.white
+        
+        webButton.setTitle("", for: UIControl.State.normal)
+        webButton.setTitle("", for: UIControl.State.highlighted)
+        fbButton.setTitle("", for: UIControl.State.normal)
+        fbButton.setTitle("", for: UIControl.State.highlighted)
     }
 
-    func updateUI(logo: String, company: String) {
+    func updateUI(logo: String, company: String, webSite: String, fbSite: String) {
         
         logoImageView.loadImage(logo)
+        logoImageView.makeCorner(radius: 20)
         
         companyLabel.text = company
+        
+        fbButton.isHidden = (fbSite.count<=0)
+        webButton.isHidden = (webSite.count<=0)
+        
+        if(fbButton.isHidden==true && webButton.isHidden==false)
+        {
+            webButtonCenterLayoutConstraint.constant = 0
+        }
+        else if(fbButton.isHidden==false && webButton.isHidden==true)
+        {
+            fbButtonCenterlayoutConstraint.constant = 0
+        }
     }
 }
